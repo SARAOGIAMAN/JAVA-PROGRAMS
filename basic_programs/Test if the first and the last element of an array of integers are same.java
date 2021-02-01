51e05d59808ec7bfa1a3ee20f568d16e1f42fe1e
Aman Saraogi <72626970+SARAOGIAMAN@users.noreply.github.com>
2021-02-01 11:23:42 +0530

    Create Test if the first and the last element of an array of integers are same.java

---
+++ basic_programs/Test if the first and the last element of an array of integers are same.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+ public class checkarrsame {
+ public static void main(String[] args)
+ {
+    int[] num_array = {50, -20, 0, 30, 40, 60, 10};
+    
+	System.out.println (num_array.length >= 2 && num_array[0] ==  num_array[num_array.length-1]);
+ }
+}
