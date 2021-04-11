497425ae317ef5690b59390176662954862dcae9
Aman Saraogi <72626970+SARAOGIAMAN@users.noreply.github.com>
2021-04-12 03:14:29 +0530

    Add files via upload

---
+++ ARRAY PROGRAMS/add_2_matrix.java
@@ -0,0 +1,32 @@
+package array;
+public class add_2_matrix  
+{  
+    public static void main(String[] args) {  
+        int rows,cols;  
+          int a[][]={  
+                          {4,5,6},  
+                          {3,4,1},  
+                          {1,2,3}  
+                       };  
+          int b[][]={  
+                          {2,0,3},  
+                         {2,3,1},  
+                         {1,1,1}  
+                     };  
+          rows=a.length;  
+        cols=a[0].length;  
+        int sum[][]=new int[rows][cols];  
+        for(int i=0;i<rows;i++){  
+            for(int j=0;j<cols;j++){  
+                sum[i][j]=a[i][j]+b[i][j];  
+            }  
+        }  
+        System.out.println("Addition of two matrices: ");  
+        for(int i=0;i<rows;i++){  
+            for(int j=0;j<cols;j++){  
+               System.out.print(sum[i][j]+" ");  
+            }  
+            System.out.println();  
+        }  
+    }  
+}    
