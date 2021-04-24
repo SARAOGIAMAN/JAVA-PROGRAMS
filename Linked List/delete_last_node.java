58b9790485fd3c58659ef674d66bbceb2020a738
Aman Saraogi <72626970+SARAOGIAMAN@users.noreply.github.com>
2021-04-25 01:11:10 +0530

    Add files via upload
    Program to delete last node from a single linked list

---
+++ Linked List/delete_last_node.java
@@ -0,0 +1,56 @@
+//Program to delete last node from a single linked list
+
+package linked_list;
+
+public class delete_last_node {
+static Node head;
+static class Node{
+	int data;
+	Node next;
+	Node(int d)
+	{
+		next=null;
+		data=d;
+	}
+	}
+void push(int data)
+{
+	Node node=new Node(data);
+	node.next=head;
+	head=node;
+}
+void delete_last()
+{
+	//Node current=head;
+	Node second_last=head;
+	while(second_last.next.next!=null)
+	{
+		second_last=second_last.next;
+	}
+	second_last.next=null;
+}
+
+static void print()
+{
+	Node n=head;
+	while(n!=null)
+	{
+		System.out.print(n.data+" ");
+		n=n.next;
+	}
+	System.out.println();
+}
+public static void main(String[] args)
+{
+	delete_last_node list=new delete_last_node();
+	list.push(1);
+	list.push(2);
+	list.push(3);
+	list.push(4);
+	System.out.println("Given linked list ");
+	list.print();
+	list.delete_last();
+	System.out.println("New linked list ");
+	list.print();
+}
+}
