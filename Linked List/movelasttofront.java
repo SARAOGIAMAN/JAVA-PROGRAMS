5f9042440d67825c3299f692dd46a1a591ba428a
Aman Saraogi <72626970+SARAOGIAMAN@users.noreply.github.com>
2021-05-01 00:18:35 +0530

    Add files via upload
    Move last node to the front in a linked list

---
+++ Linked List/movelasttofront.java
@@ -0,0 +1,65 @@
+package linked_list;
+//Moving the last element to the front in the linked list
+public class movelasttofront {
+static Node head;
+static class Node{
+	Node next;
+	int data;
+	Node(int d)
+	{
+		data=d;
+	}
+}
+void push(int data)
+{
+	Node node=new Node(data);
+	node.next=head;
+	head=node;
+}
+void move()
+{
+	if(head==null)
+		return;
+	else
+	{
+		Node seclast = null;
+        Node last = head;
+		while(last.next!=null)
+		{
+			seclast=last;
+			last=last.next;
+	}
+		seclast.next=null;
+		last.next=head;
+		head=last;
+}
+}
+void print()
+{
+	Node n=head;
+	while(n!=null)
+	{
+		System.out.print(n.data+" ");
+		n=n.next;
+	}
+}
+public static void main(String[] args)
+{
+	movelasttofront list=new movelasttofront();
+	list.push(5);
+	list.push(1);
+	list.push(6);
+	list.push(2);
+	list.push(9);
+	list.push(3);
+	System.out.println("Given linked list");
+	list.print();
+	System.out.println();
+	System.out.println("After moving");
+	list.move();
+	list.print();	
+}
+}
+
+
+
